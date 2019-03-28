ddc3aece9a6c2aae7cb9b18a3a4f6c7288f76d8f
isandratskiy <i.sandrackii@gmail.com>
2019-03-29 01:25:09 +0200

    dragndrop added, clean up

--- src/test/java/io/github/isandratskiy/tests/DragAndDropExampleTest.java
+++ src/test/java/io/github/isandratskiy/tests/DragAndDropExampleTest.java
@@ -25,7 +25,7 @@ public class DragAndDropExampleTest extends BaseTest {
 
     }
 
-    @DisplayName("can drag and drop element")
+    @DisplayName("can drag and drop first element")
     @Test
     void testCanDragAndDropElement() {
         openActivity(driver, ".view.DragAndDropDemo");
@@ -39,7 +39,7 @@ public class DragAndDropExampleTest extends BaseTest {
         );
     }
 
-    @DisplayName("can drag and drop element")
+    @DisplayName("can drag and drop hidden element")
     @Test
     void testCanDragAndDropHiddenElement() {
         openActivity(driver, ".view.DragAndDropDemo");
